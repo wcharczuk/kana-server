214e1aef85f558798915f8653fbd22619bd82005
Will Charczuk <will@blend.com>
2021-02-15 22:02:57 -0800

    updates

--- pkg/types/quiz_stats_test.go
+++ pkg/types/quiz_stats_test.go
@@ -12,5 +12,5 @@ func Test_QuizStats_PercentCorrect(t *testing.T) {
 	its := assert.New(t)
 
 	its.Zero(types.QuizStats{}.PercentCorrect())
-	its.Equal(25.0, types.QuizStats{Total: 4, Correct: 1})
+	its.Equal(25.0, types.QuizStats{Total: 4, Correct: 1}.PercentCorrect())
 }
